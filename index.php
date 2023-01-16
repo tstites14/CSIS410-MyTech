adc3b7f9353f3bcad3c24dc75cccbdf4df2060ec
Theodore Stites <argionad@gmail.com>
2023-01-16 16:43:05 -0500

    Modified color of index.php display

--- index.php
+++ index.php
@@ -6,21 +6,23 @@
         <link rel="stylesheet" type="text/css" href="index.css" />
     </head>
     <body class='content'>
-        <div class='header'>
-            <h1>Main Menu:</h1>
-            <h2>Liberty University Online CSIS 410: Web Enterprise Technologies</h2>
-        </div>
-        <div class = 'menuList'>
-            <ul>
-                <li><button>Module 1, Week 1: Foundations</button></li>
-                <li><button>Module 1, Week 1: Variables</button></li>
-                <li><button>Module 2, Week 2: Forms</button></li>
-                <li><button>Module 3, Week 3: Arrays</button></li>
-                <li><button>Module 4, Week 4: Sessions</button></li>
-                <li><button>Module 5, Week 5: CMS Sessions</button></li>
-                <li><button>Module 6, Week 6: Database</button></li>
-                <li><button>Module 8, Week 8: CMS Database</button></li>
-            </ul>
+        <div class='content'>
+            <div class='header'>
+                <h1>Main Menu:</h1>
+                <h2>Liberty University Online CSIS 410: Web Enterprise Technologies</h2>
+            </div>
+            <div class = 'menuList'>
+                <ul>
+                    <li><button>Module 1, Week 1: Foundations</button></li>
+                    <li><button>Module 1, Week 1: Variables</button></li>
+                    <li><button>Module 2, Week 2: Forms</button></li>
+                    <li><button>Module 3, Week 3: Arrays</button></li>
+                    <li><button>Module 4, Week 4: Sessions</button></li>
+                    <li><button>Module 5, Week 5: CMS Sessions</button></li>
+                    <li><button>Module 6, Week 6: Database</button></li>
+                    <li><button>Module 8, Week 8: CMS Database</button></li>
+                </ul>
+            </div>
         </div>
         <footer class= 'footer'>
             <p><?php echo "This file was last modified on " . date("F d Y, H:i:s.", filemtime("index.php")) ?></p>
