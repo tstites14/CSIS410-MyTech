fab7e8ee97e77aab6775900a45ab60ce28e586e0
Theodore Stites <argionad@gmail.com>
2023-01-16 15:02:48 -0500

    Add menu skeleton

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <html>
     <head>
-
+        <title>Main Menu</title>
     </head>
     <body>
-        
+        <h1>Main Menu:</h1>
+        <h2>Liberty University Online CSIS 410: Web Enterprise Technologies</h2>
+        <ul>
+            <li>Module 1, Week 1: Foundations</li>
+            <li>Module 1, Week 1: Variables</li>
+            <li>Module 2, Week 2: Forms</li>
+            <li>Module 3, Week 3: Arrays</li>
+            <li>Module 4, Week 4: Sessions</li>
+            <li>Module 5, Week 5: CMS Sessions</li>
+            <li>Module 6, Week 6: Database</li>
+            <li>Module 8, Week 8: CMS Database</li>
+        </ul>
     </body>
 </html>
