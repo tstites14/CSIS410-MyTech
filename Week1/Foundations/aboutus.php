e074b380cd1438763b3f56701605b8498d6c3568
Theodore Stites <argionad@gmail.com>
2023-01-19 11:59:38 -0500

    Add content and CSS to aboutus.php

---
+++ Week1/Foundations/aboutus.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>About Us</title>
+        <meta charset="utf-8"/>
+
+        <link rel="stylesheet" type="text/css" href="css/aboutus.css" />
+    </head>
+    <body class='content'>
+        <div class='content'>
+            <div class='header'>
+                <h1>MyTech Co. Ltd.</h1>
+                <h2>About Us</h2>
+            </div>
+            <div class = 'formData'>
+                <p>
+                    <?php 
+                        echo "MyTech Co. Ltd. is a sofware development studio with a focus on creating innovative and responsive mobile applications. Our mission is to provide our clients with professional mobile applications that suit their vision for their organization.";
+                        echo "<br/><br/>";
+                        echo "Based in San Francisco, California, MyTech Co. Ltd. is prepared to create professional software based on your own ideas. Our team of developers look forward to receiving your inquiry about our services!";
+                    ?>
+                </p>
+                <a class='menuList' href='menu.php'>Back</a>
+            </div>
+        </div>
+        <footer class= 'footer'>
+            <p><?php echo "This file was last modified on " . date("F d Y, H:i:s.", filemtime("menu.php")) ?></p>
+            <p>Copyright © 2023 Theodore Stites</p>
+        </footer>
+    </body>
+</html>
