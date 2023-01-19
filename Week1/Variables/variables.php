844beddfdde61da829b3bdb6a0fb15849920a16d
Theodore Stites <argionad@gmail.com>
2023-01-19 13:11:28 -0500

    Add php files for Variables assignment

---
+++ Week1/Variables/variables.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Foundations Menu</title>
+        <meta charset="utf-8"/>
+
+        <link rel="stylesheet" type="text/css" href="css/variables.css" />
+    </head>
+    <body class='content'>
+        <div class='content'>
+                <div class='header'>
+                    <h1>Week 1: Variables</h1>
+                    <h2>Liberty University Online CSIS 410: Web Enterprise Technologies</h2>
+                </div>
+        </div>
+        <footer class= 'footer'>
+            <p><?php echo "This file was last modified on " . date("F d Y, H:i:s.", filemtime("variables.php")) ?></p>
+            <p>Copyright © 2023 Theodore Stites</p>
+        </footer>
+    </body>
+</html>
