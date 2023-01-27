661f019d5743f80d5dc6b2a19c434e171142466e
Theodore Stites <argionad@gmail.com>
2023-01-27 13:17:01 -0500

    Fix incorrect timezone in footer.php

--- footer.php
+++ footer.php
@@ -17,8 +17,10 @@
 </style>
 <footer class= 'footer'>
     <?php 
+        date_default_timezone_set('America/New_York');
+
         function footer(string $url, string $fileName) {
-            echo "<p>This file was last modified on " . date("F d Y, H:i:s.", filemtime($fileName)) . "</p>";
+            echo "<p>This file was last modified on " . date("F d Y, H:i:s.", filemtime($fileName)) . " EST</p>";
             echo '<p>Copyright © 2023 Theodore Stites</p>';
             echo '<input id=\'cert\' type=\'image\' src=\'../../xhtml11.png\' name=\'validation\' alt=\'' . $url . '\' onclick=\'window.location="' . $url  . '"\'>';
             echo '</footer>';
