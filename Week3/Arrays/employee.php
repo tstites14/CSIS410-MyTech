6b9ce69e58df615eeb1092b74189cc74827e99da
Theodore Stites <argionad@gmail.com>
2023-02-01 18:21:30 -0500

    Add data to employee.php

--- Week3/Arrays/employee.php
+++ Week3/Arrays/employee.php
@@ -9,18 +9,44 @@
         <?php include "variables.php"?>
     </head>
     <body>
+        <div class='header'>
+            <h1>MyTech Co. Ltd.</h1>
+            <h2>Employee Directory</h2>
+        </div>
         <div class='content'>
             <?php
-                $employee = new Employee($_POST["id"], $_POST["name"], $_POST["title"], $_POST["department"], $_POST["degree"], $_POST["email"], $_POST["interests"], "");
+                $employee;
 
-                echo $employee->get_id();
-                echo $employee->get_name();
-                echo $employee->get_job();
-                echo $employee->get_degree();
-                echo $employee->get_department();
-                echo $employee->get_interests();
-                echo $employee->get_email();
+                foreach ($employeeArray as $emp) {
+                    if ($_POST["id"] == $emp->get_id()) {
+                        $employee = $emp;
+                        break;
+                    }
+                }
+
+                updateEmployee($employee);
+
+                function updateEmployee(Employee $employee) {
+                    $employee->set_name($_POST["name"]);
+                    $employee->set_job($_POST["title"]);
+                    $employee->set_department($_POST["department"]);
+                    $employee->set_degree($_POST["degree"]);
+                    $employee->set_email($_POST["email"]);
+                    $employee->set_interests($_POST["interests"]);
+                }
             ?>
+
+            <div class='body'>
+                <img src='<?php echo $employee->get_photo(); ?>' alt="Eric Powers">
+                <h5>
+                    <?php echo $employee->get_name(); ?>
+                    <?php echo $employee->get_job(); ?>
+                    <?php echo $employee->get_department(); ?>
+                    <?php echo $employee->get_degree(); ?>
+                    <?php echo $employee->get_email(); ?>
+                    <?php echo $employee->get_interests(); ?>
+                </h5>
+            </div>
         </div>
 
         <?php
--- Week3/Arrays/variables.php
+++ Week3/Arrays/variables.php
@@ -20,46 +20,70 @@
             $this->photo = $photo;
         }
 
+        function set_id(int $id) {
+            $this->id = $id;
+        }
         function get_id() {
             return $this->id;
         }
 
+        function set_name(string $name) {
+            $this->name = $name;
+        }
         function get_name() {
             return $this->name;
         }
 
+        function set_job(string $jobTitle) {
+            $this->jobTitle = $jobTitle;
+        }
         function get_job() {
             return $this->jobTitle;
         }
 
+        function set_department(string $department) {
+            $this->department = $department;
+        }
         function get_department() {
             return $this->department;
         }
 
+        function set_degree(string $degree) {
+            $this->degree = $degree;
+        }
         function get_degree() {
             return $this->degree;
         }
 
+        function set_email(string $email) {
+            $this->email = $email;
+        }
         function get_email() {
             return $this->email;
         }
 
+        function set_interests(string $interests) {
+            $this->interests = $interests;
+        }
         function get_interests() {
             return $this->interests;
         }
 
+        function set_photo(string $photo) {
+            $this->photo = $photo;
+        }
         function get_photo() {
             return $this->photo;
         }
     }
 
-    $ericPowers = new Employee(1, "Dr. Eric Powers", 'CEO', 'Executive', 'Ph.D. Business Administration and Management', 'epowers@mytech.com', 'Golfing', 'EricPowers.jpg');
-    $frederickNewman = new Employee(2, 'Frederick Newman', 'CIO', 'Executive', 'M.S. Information Technology', 'fnewman@mytech.com', 'Boating', 'FrederickNewman.jpg');
-    $michellePatton = new Employee(3, 'Michelle Patton', 'CFO', 'Executive', 'M.S. Accounting', 'mpatton@mytech.com', 'Movies', 'MichellePatton.jpg');
-    $hannahDelaney = new Employee(4, 'Hannah Delaney', 'Project Manager', 'Development', 'B.S. Computer Science', 'hdelaney@mytech.com', 'Woodworking', 'HannahDelaney.jpg');
-    $jamesWilliams = new Employee(5, 'James Williams', 'Senior Developer', 'Development', 'B.S. Computer Science', 'jwilliams@mytech.com', 'Open-source software', 'JamesWilliams.jpg');
-    $natalieHarvey = new Employee(6, 'Natalie Harvey', 'Developer', 'Development', 'B.S Information Technology', 'nharvey@mytech.com', 'Swimming', 'NatalieHarvey.jpg');
-    $johnEmerson = new Employee(7, 'John Emerson', 'Junior Developer', 'Development', 'B.S. Information Technology', 'jemerson@mytech.com', 'Hiking', 'JohnEmerson.jpg');
+    $ericPowers = new Employee(1, "Dr. Eric Powers", 'CEO', 'Executive', 'Ph.D. Business Administration and Management', 'epowers@mytech.com', 'Golfing', '../../Week1/Variables/img/EricPowers.jpg');
+    $frederickNewman = new Employee(2, 'Frederick Newman', 'CIO', 'Executive', 'M.S. Information Technology', 'fnewman@mytech.com', 'Boating', '../../Week1/Variables/img/FrederickNewman.jpg');
+    $michellePatton = new Employee(3, 'Michelle Patton', 'CFO', 'Executive', 'M.S. Accounting', 'mpatton@mytech.com', 'Movies', '../../Week1/Variables/img/MichellePatton.jpg');
+    $hannahDelaney = new Employee(4, 'Hannah Delaney', 'Project Manager', 'Development', 'B.S. Computer Science', 'hdelaney@mytech.com', 'Woodworking', '../../Week1/Variables/img/HannahDelaney.jpg');
+    $jamesWilliams = new Employee(5, 'James Williams', 'Senior Developer', 'Development', 'B.S. Computer Science', 'jwilliams@mytech.com', 'Open-source software', '../../Week1/Variables/img/JamesWilliams.jpg');
+    $natalieHarvey = new Employee(6, 'Natalie Harvey', 'Developer', 'Development', 'B.S Information Technology', 'nharvey@mytech.com', 'Swimming', '../../Week1/Variables/img/NatalieHarvey.jpg');
+    $johnEmerson = new Employee(7, 'John Emerson', 'Junior Developer', 'Development', 'B.S. Information Technology', 'jemerson@mytech.com', 'Hiking', '../../Week1/Variables/img/JohnEmerson.jpg');
 
     $employeeArray = array($ericPowers, $frederickNewman, $michellePatton, $hannahDelaney, $jamesWilliams, $natalieHarvey, $johnEmerson);
 ?>
