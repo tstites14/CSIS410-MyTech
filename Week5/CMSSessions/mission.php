0920c40b4b22e95165a1d8790bdc88cb8a504271
Theodore Stites <argionad@gmail.com>
2023-02-15 18:03:35 -0500

    Add text to mission.php

--- Week5/CMSSessions/mission.php
+++ Week5/CMSSessions/mission.php
@@ -13,6 +13,9 @@
 
         <div class='text'>
             <h1>Our Mission</h1>
+            <p>Software development is a tough business. We know this as well as anyone. There are hundreds of other companies that you could turn to in order to create your dream app. So what separates us from that competition? The answer lies in our customer care.</p>
+            <p>Once you purchase an app with MyTech. Co., we will contact you and get to understanding exactly what you would like us to make, and if necessary, provide feedback on how to make your app more effective and manageable.</p>
+            <p>Our mission is to provide you with the best quality apps at an affordable price. Once your app has been completed and delivered to you, our support team will be able to provide any additional assistance you may require.</p>
         </div>
 
         <?php
