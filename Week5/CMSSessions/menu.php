9784cde6d03d043fb06370b66564cc8be58dd1b9
Theodore Stites <argionad@gmail.com>
2023-02-18 14:10:30 -0500

    Add corporateresponsibility.php

--- Week5/CMSSessions/menu.php
+++ Week5/CMSSessions/menu.php
@@ -5,11 +5,13 @@
             <ul class='submenu'>
                 <li><a href="mission.php">Mission</a></li>
                 <li><a href="ourteam.php">Our Team</a></li>
+                <li><a href="corporateresponsibility.php">Corporate Responsibility</a></li>
             </ul>
             <span>&#9660;</span>
         </li>
         <li><a href="login.php">Products</a></li>
         <li><a href="helpcenter.php">Help Center</a></li>
         <li><a href="testimonials.php">Testimonials</a></li>
+        <li><a href="oursocials.php">Our Socials</a></li>
     </ul>
 </nav>
