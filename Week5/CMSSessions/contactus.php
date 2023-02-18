dac4ef7134baef74b3239e54fb0fa42517e1274c
Theodore Stites <argionad@gmail.com>
2023-02-18 14:07:09 -0500

    Add CSS to contactus.php

--- Week5/CMSSessions/contactus.php
+++ Week5/CMSSessions/contactus.php
@@ -6,23 +6,23 @@
 
         <link rel="stylesheet" type="text/css" href="css/header.css">
         <link rel="stylesheet" type="text/css" href="css/menu.css">
-
+        <link rel="stylesheet" type="text/css" href="css/contactus.css">
         <link rel="stylesheet" type="text/css" href="../../footer.css">
     </head>
     <body>
         <?php include "header.php"; ?>
 
-        <div class='text'>
+        <div class='content'>
             <h1>Contact Us</h1>
 
             <form onsubmit="alert('Thank you for your interest! We will respond as soon as we are able!')" method="post">
-                Name*: <input type="text" name="name" value="" required>
+                Name*: <input class="textbox" type="text" name="name" value="" required>
                 <br>
-                Email address*: <input type="text" name="email" value="" required>
+                Email address*: <input class="textbox" type="text" name="email" value="" required>
                 <br>
-                Comments*: <input type="text" name="comments" value="" required>
+                Comments*: <input class="textbox" type="text" name="comments" value="" required>
                 <br>
-                <input type="submit">
+                <input id="submit" type="submit">
             </form>
         </div>
 
