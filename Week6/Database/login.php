2a7654d06709a12354b08a3e99a138e5656a427f
Theodore Stites <argionad@gmail.com>
2023-02-27 16:50:57 -0500

    Fix formatting issues with login.php

--- Week6/Database/login.php
+++ Week6/Database/login.php
@@ -20,14 +20,21 @@
         <div class='content'>
             <?php include "header.php"; ?>
 
-            <h1>Log in</h1>
-            <form action="redirect.php" method="post">
-                Username*: <input class="textbox" type="text" name="username" value="" required>
-                <br>
-                Password*: <input class="textbox" type="password" name="password" value="" required>
-                <br>
-                <input class="button" type="submit" name="submit" value="Submit">
-            </form>
+            <div class='header'>
+                <h1>MyTech Co. Ltd.</h1>
+                <h2>Account Management</h2>
+            </div>
+            <div class='formData'>
+                <h1>Log in</h1>
+                <form action="redirect.php" method="post">
+                    Username*: <input class="textbox" type="text" name="username" value="" required>
+                    <br>
+                    Password*: <input class="textbox" type="password" name="password" value="" required>
+                    <br>
+                    <input class="button" type="submit" name="submit" value="Submit">
+                </form>
+            <a href="../../index.php">Back</a>
+            </div>
         </div>
 
         <?php
