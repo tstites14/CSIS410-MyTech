7fc739c670b5925eb9042ff535b01f743258ddf8
Theodore Stites <argionad@gmail.com>
2023-03-05 16:32:16 -0500

    Add database to FAQ.php

--- Week8/CMSDatabase/faq.php
+++ Week8/CMSDatabase/faq.php
@@ -11,18 +11,25 @@
         <link rel="stylesheet" type="text/css" href="../../footer.css">
     </head>
     <body>
-        <?php include "header.php"; ?>
+    <?php 
+            include "header.php"; 
+            include "dbconnection.php";
+
+            function generateFAQ() {
+                $db = new DBConnection();
+                $faq = $db->select("*", "faq");
+
+                $i = 0;
+                while ($row = $faq->fetch_assoc()) {
+                    echo "<h2>" . $row["title"] . "</h2>";
+                    echo "<p>" . $row["text"] . "</p>";
+                }
+            }
+        ?>
 
         <div class='content'>
             <h1>Frequently Asked Questions</h1>
-            <h2>How long will it take to receive my app?</h2>
-            <p>Depending on the complexity of your app, turnaround time can range from one month to three months.</p>
-            <h2>What goes into the app development process?</h2>
-            <p>Apps are complicated! They involve developers, quality control, and distribution systems. Our high-quality apps spend much of their time in development being tested to ensure that they meet our strict standards.</p>
-            <h2>Now that I have received my app, how do I distribute it on an app store?</h2>
-            <p>After your app has been sent to you, there will be an email following it shortly after that will give you specific instructions for your individual product. If you do not receive this email, feel free to <a href="contactus.php">contact us</a> and we will send it to you again!</p>
-            <h2>Something is broken with the app! What do I do?</h2>
-            <p>Please <a href="contactus.php">contact us</a> with any questions or concerns regarding your app! We are always available to assist you!</p>
+            <?php generateFAQ(); ?>
         </div>
 
         <?php
