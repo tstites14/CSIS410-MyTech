76efaf435d0c09c68d9c6667e914595e814fe2f4
Theodore Stites <argionad@gmail.com>
2023-03-05 16:44:12 -0500

    Add database to corporateresponsibility.php

--- Week8/CMSDatabase/corporateresponsibility.php
+++ Week8/CMSDatabase/corporateresponsibility.php
@@ -11,13 +11,25 @@
         <link rel="stylesheet" type="text/css" href="../../footer.css">
     </head>
     <body>
-        <?php include "header.php"; ?>
+        <?php 
+            include "header.php"; 
+            include "dbconnection.php";
+
+            function generateCR() {
+                $db = new DBConnection();
+                $cr = $db->select("text", "corporesponsibility");
+
+                while ($row = $cr->fetch_assoc()) {
+                    echo "<p>" . $row["text"] . "</p>";
+                    echo "<br>";
+                }
+            }
+        ?>
 
         <div class='content'>
             <h1>Corporate Responsibility</h1>
             <h2>Our Donations</h2>
-            <p>We have donated to <a href="thewaterproject.org">The Water Project</a> to provide water to struggling communities in Africa. As a part of our company's Christian heritage, it is important that we donate to causes that make the lives of people around the world better.</p>
-            <p>We have also donated to <a href="https://www.feedingamerica.org/">Feeding America</a>, a food bank that feeds millions of Americans every year. As Christians, it is our responsibility to follow the example of Jesus. One aspect of that is feeding the poor, and supporting Feeding America enables that.</p>
+            <?php generateCR(); ?>
         </div>
 
         <?php
