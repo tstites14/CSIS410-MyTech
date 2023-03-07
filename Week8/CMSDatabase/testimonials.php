82bccd8b05d9dabe53eb839351b4f4cb4c2985d1
Theodore Stites <argionad@gmail.com>
2023-03-07 15:39:31 -0500

    Add CRUD operations to testimonials.php

--- Week8/CMSDatabase/testimonials.php
+++ Week8/CMSDatabase/testimonials.php
@@ -15,23 +15,48 @@
             include "header.php"; 
             include "dbconnection.php";
 
+            session_start();
+
             function generateTestimonials() {
                 $db = new DBConnection();
-                $testimonials = $db->select("*", "testimonials");
+                $testimonials = $db->select("*", "testimonials", NULL, NULL, "id", "ASC");
 
-                $i = 0;
                 while ($row = $testimonials->fetch_assoc()) {
-                    echo "<h2>" . $row["name"] . ", " . $row["title"] . "</h2>";
-                    echo "<p>" . $row["comment"] . "</p>";
-                    echo "<br>";
+                    echo "<tr>";
+                    echo    "<td>";
+                    echo        "<h2>" . $row["title"] . "</h2>";
+                    echo        "<p>" . $row["text"] . "</p>";
+                    echo    "</td>";
+                    if ((int)$_SESSION["authlevel"] > 1) {
+                        echo    "<td>";
+                        echo        "<a href='dbupdate.php?itemID=" . $row["id"] . "&table=" . "testimonials" . "'><img src='img/create.svg'></a>";
+                        if ((int)$_SESSION["authlevel"] > 2) {
+                            echo        "<a href='dbdelete.php?itemID=" . $row["id"] . "&table=" . "testimonials" . "'><img src='img/delete.svg'></a>";
+                        }
+                        echo    "</td>";
+                    }
+                    echo "</tr>";
                 }
             }
         ?>
 
         <div class='content'>
             <h1>Testimonials</h1>
-            <br>
-            <?php generateTestimonials(); ?>
+            <table>
+                    <colgroup>
+                        <col span="1" style="width: 80%">
+                        <col span="1" style="width: 20%">
+                    </colgroup>
+
+                    <?php generateTestimonials(); ?>
+                </table>
+            <?php 
+                if ((int)$_SESSION["authlevel"] > 2) {
+                    echo "<form action='dbinsert.php' method='post'>";
+                    echo    "<button class='button' type='submit' name='submit' value='testimonials'><img src='img/add.svg'></button>";
+                    echo "</form>";
+                }
+            ?>
         </div>
 
         <?php
